6b3a27ca73e57058f17dfe00f3c4d07bd7e1e01f
SeanHgithub <seanhadley11@gmail.com>
2026-01-06 10:17:38 -0500

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -1,32 +1,44 @@
-
-class Roulette():
-    def __init__(self, balance, ):
+import random
+import time
+class Roulette:
+    def __init__(self, balance):
         self.balance = balance
-    wheel = [
-    "0G",
-    "1R", "2B", "3R", "4B", "5R", "6B",
-    "7R", "8B", "9R", "10B", "11B", "12R",
-    "13B", "14R", "15B", "16R", "17B", "18R",
-    "19R", "20B", "21R", "22B", "23R", "24B",
-    "25R", "26B", "27R", "28B", "29B", "30R",
-    "31B", "32R", "33B", "34R", "35B", "36R"
-    ]
-    def buy_chips(self):
-    casino_chips = [
-        "White = $1",
-        "Red = $5",
-        "Blue/Brown/Orange = $10",
-        "Green = $25",
-        "Black = $100",
-        "Purple = $500",
-        "Yellow/Orange/Gray = $1000",
-        "Orange/Gray/Red-White-Blue = $5000"
-    ]
-    for chip in casino_chips:
-    print(chip)
-        chip = input("what type of chip would you like")
     def single_number(self):
-        dealer = input("Choose a number")
+        while again == True:
+            wheel = [
+                    "0G",
+                    "1R", "2B", "3R", "4B", "5R", "6B",
+                    "7R", "8B", "9R", "10B", "11B", "12R",
+                    "13B", "14R", "15B", "16R", "17B", "18R",
+                    "19R", "20B", "21R", "22B", "23R", "24B",
+                    "25R", "26B", "27R", "28B", "29B", "30R",
+                    "31B", "32R", "33B", "34R", "35B", "36R"
+                    ]
+            bet = input("How much do you want to bet?")
+            if bet > self.balance:
+                print("You don't have enough money in your balance as of right now.")
+                again == True
+            gamble = input("Choose a number")
+            spins = random.randint(72,96)
+            for i in range(spins):
+                print(wheel[i % 37])
+                time.sleep(0.05)
+            number = wheel[spins % 37]       
+            if gamble == number:
+                balance += bet * 35
+                print(f"Congrats! You bet ${bet} and won ${bet * 35}")
+            else:
+                print(f"Unfortunately you lost ${bet}")
+                balance -= bet
+            again = input(f"Your balance is now {self.balance}, do you want to play again?")
+            if again.lower() == "yes":
+                again == True
+            else:
+                again == False
+            
+
+            
+        
 
     
 
@@ -60,3 +72,20 @@ class Roulette():
 #    18 numbers (19-36): pays even money. 
 #    Red or black: pays even money. 
 #    Odd or even: bets pay even money.
+"""     def buy_chips(self):
+        casino_chips = [
+            "White = $1",
+            "Red = $5",
+            "Blue/Brown/Orange = $10",
+            "Green = $25",
+            "Black = $100",
+            "Purple = $500",
+            "Yellow = $1000",
+            "Orange = $5000"
+        ]
+        for chip in casino_chips:
+            print(chip)
+        chips = input("what type of chip would you like")
+        if chips.lower() == "white":
+            if self.balance >= 1:
+                quantity = input("How many?") """
