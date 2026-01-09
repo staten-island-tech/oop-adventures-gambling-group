af7745c661940d20431f82093d1a70299a0e23e5
SeanHgithub <seanhadley11@gmail.com>
2026-01-09 10:17:39 -0500

    fixerrors + red/black + 18 num bets

--- roulette.py
+++ roulette.py
@@ -13,7 +13,9 @@ class Roulette:
 3. Triple number bet
 4. Four number bet
 5. Five number bet
-6. Six number bet                                                                                 
+6. Six number bet
+7. Dozen bet
+8. Red/Black                                                                                                            
 """)
             if choose == "1":
                 self.single_number()
@@ -32,7 +34,13 @@ class Roulette:
                 again5 = False
             elif choose == "6":
                 self.six_numbers()
-                again5 = False                
+                again5 = False       
+            elif choose == "7":
+                self.dozen_bet()
+                again5 = False         
+            elif choose == "8":
+                self.red_black()
+                again5 = False
             else:
                 print("Invalid option, you must choose a number.")
                 again5 = True
@@ -87,7 +95,7 @@ class Roulette:
 
         spins = random.randint(74,111)
         raterate = 1.00008
-        rate = 1.01
+        rate = 1.0075
         x = 0.07
         for i in range(spins):
             x *= rate
@@ -155,7 +163,7 @@ class Roulette:
             numbers.append(gamble)
         spins = random.randint(111,148)
         raterate = 1.00008
-        rate = 1.01
+        rate = 1.0075
         x = 0.07
         for i in range(spins):
             x *= rate
@@ -223,7 +231,7 @@ class Roulette:
             numbers.append(gamble)
         spins = random.randint(111,148)
         raterate = 1.00008
-        rate = 1.01
+        rate = 1.0075
         x = 0.07
         for i in range(spins):
             x *= rate
@@ -291,7 +299,7 @@ class Roulette:
             numbers.append(gamble)
         spins = random.randint(111,148)
         raterate = 1.00008
-        rate = 1.01
+        rate = 1.0075
         x = 0.07
         for i in range(spins):
             x *= rate
@@ -359,7 +367,7 @@ class Roulette:
             numbers.append(gamble)
         spins = random.randint(111,148)
         raterate = 1.00008
-        rate = 1.01
+        rate = 1.0075
         x = 0.07
         for i in range(spins):
             x *= rate
@@ -427,7 +435,7 @@ class Roulette:
             numbers.append(gamble)
         spins = random.randint(111,148)
         raterate = 1.00008
-        rate = 1.01
+        rate = 1.075
         x = 0.07
         for i in range(spins):
             x *= rate
@@ -484,7 +492,7 @@ class Roulette:
             gamble = input("""Choose your dozen:
 1. 1-12
 2. 13-24
-3.25-36""")
+3. 25-36""")
             if gamble == "1":
                 numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
             elif gamble == "2":
@@ -497,7 +505,7 @@ class Roulette:
             break
         spins = random.randint(111,148)
         raterate = 1.00008
-        rate = 1.01
+        rate = 1.0075
         x = 0.07
         for i in range(spins):
             x *= rate
@@ -528,6 +536,136 @@ class Roulette:
                 print("Invalid option. (1/2)")
                 continue
 
+
+    def red_black(self):
+        wheel = [
+                "0G",
+                "1R", "2B", "3R", "4B", "5R", "6B",
+                "7R", "8B", "9R", "10B", "11B", "12R",
+                "13B", "14R", "15B", "16R", "17B", "18R",
+                "19R", "20B", "21R", "22B", "23R", "24B",
+                "25R", "26B", "27R", "28B", "29B", "30R",
+                "31B", "32R", "33B", "34R", "35B", "36R"
+                ]
+        while True:
+            bet = input("How much do you want to bet? ")
+            if not bet.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            bet = int(bet)
+            if bet > self.balance or bet < 1:
+                print("Invalid bet amount")
+                continue
+            break
+        while True:
+            gamble = input("Red or Black?")
+            if gamble.lower() == "red":
+                color = "R"
+                break
+            elif gamble.lower() == "black":
+                color = "B"
+                break
+            else:
+                print("Invalid option, choose again.")
+                continue
+        spins = random.randint(111,148)
+        raterate = 1.00005
+        rate = 1.0075
+        x = 0.07
+        for i in range(spins):
+            x *= rate
+            rate *= raterate
+            print(wheel[i % 37])
+            time.sleep(x)
+            if i >= spins - 6:
+                raterate *= 1.08
+        number = wheel[(spins - 1) % 37]       
+        if color in number:
+            self.balance += bet
+            print(f"Congrats! You bet ${bet} and won ${bet}")
+        else:
+            print(f"Unfortunately you lost ${bet}")
+            self.balance -= bet
+        self.broke()
+        while True:
+            again_ask = input(f"""Your balance is now ${self.balance}, choose an option:
+1. Play again
+2. Play a different game
+""")
+            if again_ask == "1":
+                return self.red_black()
+            elif again_ask.lower() == "2":
+                self.home_page()
+                return
+            else:
+                print("Invalid option. (1/2)")
+                continue
+    def half_numbers(self):
+        wheel = [
+                "0G",
+                "1R", "2B", "3R", "4B", "5R", "6B",
+                "7R", "8B", "9R", "10B", "11B", "12R",
+                "13B", "14R", "15B", "16R", "17B", "18R",
+                "19R", "20B", "21R", "22B", "23R", "24B",
+                "25R", "26B", "27R", "28B", "29B", "30R",
+                "31B", "32R", "33B", "34R", "35B", "36R"
+                ]
+        while True:
+            bet = input("How much do you want to bet? ")
+            if not bet.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            bet = int(bet)
+            if bet > self.balance or bet < 1:
+                print("Invalid bet amount")
+                continue
+            break
+        numbers = []
+        while True:
+            gamble = input("""Choose your half:
+1. 1-18
+2. 19-36
+""")
+            if gamble == "1":
+                numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+            elif gamble == "2":
+                numbers = [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+            else:
+                print("Invalid option, you must choose a number.")
+                continue
+            break
+        spins = random.randint(111,148)
+        raterate = 1.00008
+        rate = 1.0075
+        x = 0.07
+        for i in range(spins):
+            x *= rate
+            rate *= raterate
+            print(wheel[i % 37])
+            time.sleep(x)
+            if i >= spins - 6:
+                raterate *=1.08
+        number = wheel[(spins - 1) % 37]       
+        if number[:-1] in numbers:
+            self.balance += bet
+            print(f"Congrats! You bet ${bet} and won ${bet}")
+        else:
+            print(f"Unfortunately you lost ${bet}")
+            self.balance -= bet
+        self.broke()
+        while True:
+            again_ask = input(f"""Your balance is now ${self.balance}, choose an option:
+1. Play again
+2. Play a different game
+""")
+            if again_ask == "1":
+                return self.dozen_bet()
+            elif again_ask.lower() == "2":
+                self.home_page()
+                return
+            else:
+                print("Invalid option. (1/2)")
+                continue
 game = Roulette(10000)            
 game.home_page()
             
@@ -561,9 +699,9 @@ game.home_page()
 #    Six number bets: pays 5 to 1. Example: 7, 8, 9, 10, 11, 12. Also called a “line.”
 #    Twelve numbers or dozens: (first, second, third dozen) pays 2 to 1.
 #    Column bet (12 numbers in a row): pays 2 to 1. 
+#    18 numbers (1-18): pays even money. 
 #    18 numbers (19-36): pays even money. 
-#    Red or black (1-18): pays even money. 
-#    18 number: pays even money. 
+#    Red or black: pays even money. 
 #    Odd or even: bets pay even money.
 """     def buy_chips(self):
         casino_chips = [
