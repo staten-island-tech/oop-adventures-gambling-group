fb5f61f8a9deb37c784aea487d03c99f1a051a37
SeanHgithub <seanhadley11@gmail.com>
2026-01-08 22:40:24 -0500

    fixerrors + dozenbets

--- roulette.py
+++ roulette.py
@@ -10,6 +10,10 @@ class Roulette:
             choose = input("""Choose what you want to play(1,2,etc.):
 1. Single number bet
 2. Double number bet
+3. Triple number bet
+4. Four number bet
+5. Five number bet
+6. Six number bet                                                                                 
 """)
             if choose == "1":
                 self.single_number()
@@ -17,10 +21,23 @@ class Roulette:
             elif choose == "2":
                 self.double_number()
                 again5 = False
+            elif choose == "3":
+                self.triple_number()
+                again5 = False
+            elif choose == "4":
+                self.four_numbers()
+                again5 = False
+            elif choose == "5":
+                self.five_numbers()
+                again5 = False
+            elif choose == "6":
+                self.six_numbers()
+                again5 = False                
             else:
                 print("Invalid option, you must choose a number.")
                 again5 = True
     
+
     def broke(self):
         if self.balance <= 1:
             print("""Your balance is $0. You will be redirected to the home page.
@@ -35,6 +52,8 @@ class Roulette:
             """)
             time.sleep(1.5)
             self.home_page()
+
+
     def single_number(self):
         wheel = [
                 "0G",
@@ -61,7 +80,7 @@ class Roulette:
                 print("Error. Invalid input, must be an integer. Try again")
                 continue
             gamble = int(gamble)
-            if gamble < 0 or gamble > 36:
+            if gamble < 1 or gamble > 36:
                 print("Invalid number. Try again, 0-36.")
                 continue
             break
@@ -90,14 +109,16 @@ class Roulette:
 1. Play again
 2. Play a different game
 """)
-            if again_ask == 1:
-                break
+            if again_ask == "1":
+                return self.single_number()
             elif again_ask.lower() == "2":
                 self.home_page()
                 return
             else:
                 print("Invalid option. (1/2)")
                 continue
+
+
     def double_number(self):
         wheel = [
                 "0G",
@@ -125,7 +146,7 @@ class Roulette:
                 print("Error. Invalid input, must be an integer. Try again")
                 continue
             gamble = int(gamble)
-            if gamble < 0 or gamble > 36:
+            if gamble < 1 or gamble > 36:
                 print("Invalid number. Try again, 0-36.")
                 continue
             if gamble in numbers:
@@ -133,7 +154,7 @@ class Roulette:
                 continue
             numbers.append(gamble)
         spins = random.randint(111,148)
-        raterate = 1.00005
+        raterate = 1.00008
         rate = 1.01
         x = 0.07
         for i in range(spins):
@@ -156,8 +177,76 @@ class Roulette:
 1. Play again
 2. Play a different game
 """)
-            if again_ask == 1:
-                break
+            if again_ask == "1":
+                return self.double_number()
+            elif again_ask == "2":
+                self.home_page()
+                return
+            else:
+                print("Invalid option. (1/2)")
+                continue
+
+
+    def triple_number(self):
+        wheel = [
+                "0G",
+                "1R", "2B", "3R", "4B", "5R", "6B",
+                "7R", "8B", "9R", "10B", "11B", "12R",
+                "13B", "14R", "15B", "16R", "17B", "18R",
+                "19R", "20B", "21R", "22B", "23R", "24B",
+                "25R", "26B", "27R", "28B", "29B", "30R",
+                "31B", "32R", "33B", "34R", "35B", "36R"
+                ]
+        while True:
+            bet = input("How much do you want to bet? ")
+            if not bet.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            bet = int(bet)
+            if bet > self.balance or bet < 1:
+                print("Invalid bet amount")
+                continue
+            break
+        numbers = []
+        while len(numbers) < 3:
+            gamble = input(f"Choose number #{len(numbers) + 1}: ")
+            if not gamble.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            gamble = int(gamble)
+            if gamble < 1 or gamble > 36:
+                print("Invalid number. Try again, 0-36.")
+                continue
+            if gamble in numbers:
+                print("You already chose that number. Choose another.")
+                continue
+            numbers.append(gamble)
+        spins = random.randint(111,148)
+        raterate = 1.00008
+        rate = 1.01
+        x = 0.07
+        for i in range(spins):
+            x *= rate
+            rate *= raterate
+            print(wheel[i % 37])
+            time.sleep(x)
+            if i >= spins - 6:
+                raterate *=1.08
+        number = wheel[(spins - 1) % 37]       
+        if number[:-1] in numbers:
+            self.balance += bet * 11
+            print(f"Congrats! You bet ${bet} and won ${bet * 11}")
+        else:
+            print(f"Unfortunately you lost ${bet}")
+            self.balance -= bet
+        self.broke()
+        while True:
+            again_ask = input(f"""Your balance is now ${self.balance}, choose an option:
+1. Play again
+2. Play a different game
+""")
+            if again_ask == "1":
+                return self.triple_number()
             elif again_ask.lower() == "2":
                 self.home_page()
                 return
@@ -165,6 +254,280 @@ class Roulette:
                 print("Invalid option. (1/2)")
                 continue
         
+
+    def four_numbers(self):
+        wheel = [
+                "0G",
+                "1R", "2B", "3R", "4B", "5R", "6B",
+                "7R", "8B", "9R", "10B", "11B", "12R",
+                "13B", "14R", "15B", "16R", "17B", "18R",
+                "19R", "20B", "21R", "22B", "23R", "24B",
+                "25R", "26B", "27R", "28B", "29B", "30R",
+                "31B", "32R", "33B", "34R", "35B", "36R"
+                ]
+        while True:
+            bet = input("How much do you want to bet? ")
+            if not bet.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            bet = int(bet)
+            if bet > self.balance or bet < 1:
+                print("Invalid bet amount")
+                continue
+            break
+        numbers = []
+        while len(numbers) < 4:
+            gamble = input(f"Choose number #{len(numbers) + 1}: ")
+            if not gamble.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            gamble = int(gamble)
+            if gamble < 1 or gamble > 36:
+                print("Invalid number. Try again, 0-36.")
+                continue
+            if gamble in numbers:
+                print("You already chose that number. Choose another.")
+                continue
+            numbers.append(gamble)
+        spins = random.randint(111,148)
+        raterate = 1.00008
+        rate = 1.01
+        x = 0.07
+        for i in range(spins):
+            x *= rate
+            rate *= raterate
+            print(wheel[i % 37])
+            time.sleep(x)
+            if i >= spins - 6:
+                raterate *=1.08
+        number = wheel[(spins - 1) % 37]       
+        if number[:-1] in numbers:
+            self.balance += bet * 8
+            print(f"Congrats! You bet ${bet} and won ${bet * 8}")
+        else:
+            print(f"Unfortunately you lost ${bet}")
+            self.balance -= bet
+        self.broke()
+        while True:
+            again_ask = input(f"""Your balance is now ${self.balance}, choose an option:
+1. Play again
+2. Play a different game
+""")
+            if again_ask == "1":
+                return self.four_numbers()
+            elif again_ask.lower() == "2":
+                self.home_page()
+                return
+            else:
+                print("Invalid option. (1/2)")
+                continue
+
+
+    def five_numbers(self):
+        wheel = [
+                "0G",
+                "1R", "2B", "3R", "4B", "5R", "6B",
+                "7R", "8B", "9R", "10B", "11B", "12R",
+                "13B", "14R", "15B", "16R", "17B", "18R",
+                "19R", "20B", "21R", "22B", "23R", "24B",
+                "25R", "26B", "27R", "28B", "29B", "30R",
+                "31B", "32R", "33B", "34R", "35B", "36R"
+                ]
+        while True:
+            bet = input("How much do you want to bet? ")
+            if not bet.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            bet = int(bet)
+            if bet > self.balance or bet < 1:
+                print("Invalid bet amount")
+                continue
+            break
+        numbers = []
+        while len(numbers) < 5:
+            gamble = input(f"Choose number #{len(numbers) + 1}: ")
+            if not gamble.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            gamble = int(gamble)
+            if gamble < 1 or gamble > 36:
+                print("Invalid number. Try again, 0-36.")
+                continue
+            if gamble in numbers:
+                print("You already chose that number. Choose another.")
+                continue
+            numbers.append(gamble)
+        spins = random.randint(111,148)
+        raterate = 1.00008
+        rate = 1.01
+        x = 0.07
+        for i in range(spins):
+            x *= rate
+            rate *= raterate
+            print(wheel[i % 37])
+            time.sleep(x)
+            if i >= spins - 6:
+                raterate *=1.08
+        number = wheel[(spins - 1) % 37]       
+        if number[:-1] in numbers:
+            self.balance += bet * 6
+            print(f"Congrats! You bet ${bet} and won ${bet * 6}")
+        else:
+            print(f"Unfortunately you lost ${bet}")
+            self.balance -= bet
+        self.broke()
+        while True:
+            again_ask = input(f"""Your balance is now ${self.balance}, choose an option:
+1. Play again
+2. Play a different game
+""")
+            if again_ask == "1":
+                return self.five_numbers()
+            elif again_ask.lower() == "2":
+                self.home_page()
+                return
+            else:
+                print("Invalid option. (1/2)")
+                continue
+
+
+    def six_numbers(self):
+        wheel = [
+                "0G",
+                "1R", "2B", "3R", "4B", "5R", "6B",
+                "7R", "8B", "9R", "10B", "11B", "12R",
+                "13B", "14R", "15B", "16R", "17B", "18R",
+                "19R", "20B", "21R", "22B", "23R", "24B",
+                "25R", "26B", "27R", "28B", "29B", "30R",
+                "31B", "32R", "33B", "34R", "35B", "36R"
+                ]
+        while True:
+            bet = input("How much do you want to bet? ")
+            if not bet.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            bet = int(bet)
+            if bet > self.balance or bet < 1:
+                print("Invalid bet amount")
+                continue
+            break
+        numbers = []
+        while len(numbers) < 6:
+            gamble = input(f"Choose number #{len(numbers) + 1}: ")
+            if not gamble.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            gamble = int(gamble)
+            if gamble < 1 or gamble > 36:
+                print("Invalid number. Try again, 0-36.")
+                continue
+            if gamble in numbers:
+                print("You already chose that number. Choose another.")
+                continue
+            numbers.append(gamble)
+        spins = random.randint(111,148)
+        raterate = 1.00008
+        rate = 1.01
+        x = 0.07
+        for i in range(spins):
+            x *= rate
+            rate *= raterate
+            print(wheel[i % 37])
+            time.sleep(x)
+            if i >= spins - 6:
+                raterate *=1.08
+        number = wheel[(spins - 1) % 37]       
+        if number[:-1] in numbers:
+            self.balance += bet * 5
+            print(f"Congrats! You bet ${bet} and won ${bet * 5}")
+        else:
+            print(f"Unfortunately you lost ${bet}")
+            self.balance -= bet
+        self.broke()
+        while True:
+            again_ask = input(f"""Your balance is now ${self.balance}, choose an option:
+1. Play again
+2. Play a different game
+""")
+            if again_ask == "1":
+                return self.six_numbers()
+            elif again_ask.lower() == "2":
+                self.home_page()
+                return
+            else:
+                print("Invalid option. (1/2)")
+                continue
+
+
+    def dozen_bet(self):
+        wheel = [
+                "0G",
+                "1R", "2B", "3R", "4B", "5R", "6B",
+                "7R", "8B", "9R", "10B", "11B", "12R",
+                "13B", "14R", "15B", "16R", "17B", "18R",
+                "19R", "20B", "21R", "22B", "23R", "24B",
+                "25R", "26B", "27R", "28B", "29B", "30R",
+                "31B", "32R", "33B", "34R", "35B", "36R"
+                ]
+        while True:
+            bet = input("How much do you want to bet? ")
+            if not bet.isdigit():
+                print("Error. Invalid input, must be an integer. Try again")
+                continue
+            bet = int(bet)
+            if bet > self.balance or bet < 1:
+                print("Invalid bet amount")
+                continue
+            break
+        numbers = []
+        while True:
+            gamble = input("""Choose your dozen:
+1. 1-12
+2. 13-24
+3.25-36""")
+            if gamble == "1":
+                numbers = [1,2,3,4,5,6,7,8,9,10,11,12]
+            elif gamble == "2":
+                numbers = [13,14,15,16,17,18,19,20,21,22,23,24]
+            elif gamble == "3":
+                numbers = [25,26,27,28,29,30,31,32,33,34,35,36]
+            else:
+                print("Invalid option, you must choose a number.")
+                continue
+            break
+        spins = random.randint(111,148)
+        raterate = 1.00008
+        rate = 1.01
+        x = 0.07
+        for i in range(spins):
+            x *= rate
+            rate *= raterate
+            print(wheel[i % 37])
+            time.sleep(x)
+            if i >= spins - 6:
+                raterate *=1.08
+        number = wheel[(spins - 1) % 37]       
+        if number[:-1] in numbers:
+            self.balance += bet * 2
+            print(f"Congrats! You bet ${bet} and won ${bet * 2}")
+        else:
+            print(f"Unfortunately you lost ${bet}")
+            self.balance -= bet
+        self.broke()
+        while True:
+            again_ask = input(f"""Your balance is now ${self.balance}, choose an option:
+1. Play again
+2. Play a different game
+""")
+            if again_ask == "1":
+                return self.dozen_bet()
+            elif again_ask.lower() == "2":
+                self.home_page()
+                return
+            else:
+                print("Invalid option. (1/2)")
+                continue
+
 game = Roulette(10000)            
 game.home_page()
             
@@ -198,9 +561,9 @@ game.home_page()
 #    Six number bets: pays 5 to 1. Example: 7, 8, 9, 10, 11, 12. Also called a “line.”
 #    Twelve numbers or dozens: (first, second, third dozen) pays 2 to 1.
 #    Column bet (12 numbers in a row): pays 2 to 1. 
-#    18 numbers (1-18): pays even money. 
 #    18 numbers (19-36): pays even money. 
-#    Red or black: pays even money. 
+#    Red or black (1-18): pays even money. 
+#    18 number: pays even money. 
 #    Odd or even: bets pay even money.
 """     def buy_chips(self):
         casino_chips = [
